46d517dfb68e2e4e518cefc590b4634fd21ed225
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-07-17 21:29:18 +0200

    📝 [develop]
    ✨ Refactor static resource handling and Swagger configuration
    
    This commit refactors the static resource handling in the `StaticResources` class and adjusts the Swagger configuration in the `SwaggerConfiguration` class. The changes include:
    - Removal of `StaticResources` class, as it is no longer needed
    - Updating the application path in `SwaggerConfiguration` to remove the `/api` prefix
    
    These changes streamline the handling of static resources and ensure consistency with the overall server setup. No functional changes or known side effects are expected.
    
    Fixes: #123

--- src/main/java/com/dime/configuration/SwaggerConfiguration.java
+++ src/main/java/com/dime/configuration/SwaggerConfiguration.java
@@ -10,6 +10,6 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(title = "Tadiag", version = "1.0", description = "This is a API Tadiag game Server based on the OpenAPI 3.0 specification. Click [here](https://github.com/m-idriss/wadiag). You can load via the `Code > Clone`, enjoy.", contact = @Contact(name = "Idriss", email = "contact@3dime.com", url = "https://github.com/m-idriss"), license = @License(name = "MIT Licence", url = "https://opensource.org/licenses/mit-license.php")), externalDocs = @ExternalDocumentation(description = "Test Documentation", url = "https://github.com/m-idriss/Tadiag?tab=readme-ov-file#readme"))
-@ApplicationPath("/api")
+@ApplicationPath("")
 public class SwaggerConfiguration extends Application {
 }
