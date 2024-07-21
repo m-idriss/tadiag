9f18798d147a35d29dd631151c14c982734eb592
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-07-21 23:07:21 +0200

    feat: add tests on termResourceTest

--- src/test/java/com/dime/term/TermResourceTest.java
+++ src/test/java/com/dime/term/TermResourceTest.java
@@ -1,9 +1,11 @@
 package com.dime.term;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.matchesPattern;
 
@@ -23,20 +25,12 @@ class TermResourceTest {
                 .body("word", equalTo("health"))
                 .body("synonyms", hasSize(1))
                 .body("synonyms", hasItem("wellness"))
+                .body("_links", allOf(
+                        hasKey("self-by-word"),
+                        hasKey("self"),
+                        hasKey("list")))
                 .body("_links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/health"))
                 .body("_links.self.href", matchesPattern("http://[^/]+/api/v1/terms/1"))
-                .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
-
-    }
-
-    @Test
-    void testListAllEndpoint() {
-
-        given()
-                .when().get("api/v1/terms")
-                .then()
-                .statusCode(200)
-                .body("_embedded.terms", empty())
                 .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
 
     }
