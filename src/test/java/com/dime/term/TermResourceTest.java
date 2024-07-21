9cbb36169fcd5be96a0fa282574e5deb77894d3c
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-07-21 22:53:35 +0200

    feat: Add unit tests for TermResource endpoints

--- src/test/java/com/dime/term/TermResourceTest.java
+++ src/test/java/com/dime/term/TermResourceTest.java
@@ -1,9 +1,12 @@
 package com.dime.term;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.matchesPattern;
 
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -11,26 +14,41 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 class TermResourceTest {
     @Test
-    @Order(1)
-    void testHelloEndpoint() {
+    void testGetbyWordEndpoint() {
         given()
                 .when().get("api/v1/terms/health")
                 .then()
                 .statusCode(200)
-                .body(containsString("\"id\":"))
-                .body(containsString("\"word\":\"health\",\"synonyms\":[\"wellness\"]"));
+                .body("id", equalTo(1))
+                .body("word", equalTo("health"))
+                .body("synonyms", hasSize(1))
+                .body("synonyms", hasItem("wellness"))
+                .body("_links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/health"))
+                .body("_links.self.href", matchesPattern("http://[^/]+/api/v1/terms/1"))
+                .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
 
     }
 
     @Test
-    @Order(2)
     void testListAllEndpoint() {
+
         given()
                 .when().get("api/v1/terms")
                 .then()
                 .statusCode(200)
-                .body(containsString("\"id\":"))
-                .body(containsString("\"word\":\"health\",\"synonyms\":[\"wellness\"]"));
+                .body("_embedded.terms", empty())
+                .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
+
+    }
+
+    @Test
+    void testEmptyListAllEndpoint() {
+        given()
+                .when().get("api/v1/terms")
+                .then()
+                .statusCode(200)
+                .body("_embedded.terms", empty())
+                .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
 
     }
 
