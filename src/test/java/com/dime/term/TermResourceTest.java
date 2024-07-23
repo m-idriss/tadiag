47e7c6f82989151ba4157a54a4ac2228336a1e4d
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-07-23 21:46:37 +0200

    ✅ feat: improve unit tests for TermResource endpoints

--- src/test/java/com/dime/term/TermResourceTest.java
+++ src/test/java/com/dime/term/TermResourceTest.java
@@ -1,8 +1,7 @@
 package com.dime.term;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasKey;
@@ -14,9 +13,10 @@ import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
-class TermResourceTest {
+public class TermResourceTest {
+
     @Test
-    void testGetTermByWord() {
+    public void testGetTermByWord() {
         given()
                 .when().get("api/v1/terms/health")
                 .then()
@@ -32,18 +32,36 @@ class TermResourceTest {
                 .body("_links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/health"))
                 .body("_links.self.href", matchesPattern("http://[^/]+/api/v1/terms/1"))
                 .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
-
     }
 
     @Test
-    void testListAllTerms() {
+    public void testListAllTermsWithTwoTerms() {
+        given()
+                .when().get("api/v1/terms/health").then().statusCode(200);
+        given()
+                .when().get("api/v1/terms/TRY").then().statusCode(200);
+
+        given()
+                .when().get("api/v1/terms/Test").then().statusCode(200);
+
         given()
                 .when().get("api/v1/terms")
                 .then()
                 .statusCode(200)
-                .body("_embedded.terms", empty())
+                .body("_embedded", hasKey("terms"))
+                .body("_embedded.terms", hasSize(3))
+                .body("_embedded.terms[0].id", equalTo(1))
+                .body("_embedded.terms[0].word", equalTo("health"))
+                .body("_embedded.terms[0].synonyms", hasSize(1))
+                .body("_embedded.terms[0].synonyms", hasItem("wellness"))
+                .body("_embedded.terms[1].id", equalTo(2))
+                .body("_embedded.terms[1].word", equalTo("try"))
+                .body("_embedded.terms[1].synonyms", hasSize(20))
+                .body("_embedded.terms[1].synonyms", hasItem("test"))
+                .body("_embedded.terms[2].id", equalTo(3))
+                .body("_embedded.terms[2].word", equalTo("test"))
+                .body("_embedded.terms[2].synonyms", hasSize(16))
+                .body("_embedded.terms[2].synonyms", hasItem("run"))
                 .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
-
     }
-
 }
