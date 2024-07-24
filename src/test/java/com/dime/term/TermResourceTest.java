bd0a2e372af400be3f0f3f21bac8f88b7f797ca3
Idriss <pro.mohamady@gmail.com>
2024-07-24 10:33:50 +0200

    update termResourceTest with constants

--- src/test/java/com/dime/term/TermResourceTest.java
+++ src/test/java/com/dime/term/TermResourceTest.java
@@ -1,5 +1,8 @@
 package com.dime.term;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
@@ -7,24 +10,28 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.matchesPattern;
-
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.junit.QuarkusTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class TermResourceTest {
 
+  public static final String TERMS_ENDPOINT = "api/v1/terms";
+  public static final String DOMAIN = "http://[^/]+/";
+
   @Test
-  public void testGetTermByWord() {
+  public void testGetTermById() {
 
-    int healthId = given()
-        .when().get("api/v1/terms/health")
-        .then()
-        .statusCode(200).extract().path("id");
+    // call with word
+    int healthId = given().when().get(TERMS_ENDPOINT + "/health").then().statusCode(200).extract().path("id");
 
+    // repeat call with word
+    int healthIdStateless = given().when().get(TERMS_ENDPOINT + "/health").then().statusCode(200).extract().path("id");
+
+    assertEquals(healthId, healthIdStateless);
+
+    // call by id and check response
     given()
-        .when().get("api/v1/terms/health")
+        .when().get(TERMS_ENDPOINT + "/" + healthId)
         .then()
         .statusCode(200)
         .body("id", equalTo(healthId))
@@ -35,20 +42,45 @@ public class TermResourceTest {
             hasKey("self-by-word"),
             hasKey("self"),
             hasKey("list")))
-        .body("_links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/health"))
-        .body("_links.self.href", matchesPattern("http://[^/]+/api/v1/terms/" + healthId))
-        .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
+        .body("_links.self-by-word.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/health"))
+        .body("_links.self.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/" + healthId))
+        .body("_links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT));
   }
 
   @Test
-  public void testListAllTermsWithTwoTerms() {
+  public void testGetTermByWord() {
 
-    int healthId = given().when().get("api/v1/terms/health").then().statusCode(200).extract().path("id");
-    int tryId = given().when().get("api/v1/terms/TRY").then().statusCode(200).extract().path("id");
-    int testId = given().when().get("api/v1/terms/Test").then().statusCode(200).extract().path("id");
+    int healthId = given()
+        .when().get(TERMS_ENDPOINT + "/health")
+        .then()
+        .statusCode(200).extract().path("id");
 
     given()
-        .when().get("api/v1/terms")
+        .when().get(TERMS_ENDPOINT + "/health")
+        .then()
+        .statusCode(200)
+        .body("id", equalTo(healthId))
+        .body("word", equalTo("health"))
+        .body("synonyms", hasSize(1))
+        .body("synonyms", hasItem("wellness"))
+        .body("_links", allOf(
+            hasKey("self-by-word"),
+            hasKey("self"),
+            hasKey("list")))
+        .body("_links.self-by-word.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/health"))
+        .body("_links.self.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/" + healthId))
+        .body("_links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT));
+  }
+
+  @Test
+  public void testListAllTerms() {
+
+    int healthId = given().when().get(TERMS_ENDPOINT + "/health").then().statusCode(200).extract().path("id");
+    int tryId = given().when().get(TERMS_ENDPOINT + "/TRY").then().statusCode(200).extract().path("id");
+    int testId = given().when().get(TERMS_ENDPOINT + "/Test").then().statusCode(200).extract().path("id");
+
+    given()
+        .when().get(TERMS_ENDPOINT)
         .then()
         .statusCode(200)
         .body("_embedded", hasKey("terms"))
@@ -57,23 +89,23 @@ public class TermResourceTest {
         .body("_embedded.terms[0].word", equalTo("health"))
         .body("_embedded.terms[0].synonyms", hasSize(1))
         .body("_embedded.terms[0].synonyms", hasItem("wellness"))
-        .body("_embedded.terms[0]._links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/health"))
-        .body("_embedded.terms[0]._links.self.href", matchesPattern("http://[^/]+/api/v1/terms/" + healthId))
-        .body("_embedded.terms[0]._links.list.href", matchesPattern("http://[^/]+/api/v1/terms"))
+        .body("_embedded.terms[0]._links.self-by-word.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/health"))
+        .body("_embedded.terms[0]._links.self.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/" + healthId))
+        .body("_embedded.terms[0]._links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT))
         .body("_embedded.terms[1].id", equalTo(tryId))
         .body("_embedded.terms[1].word", equalTo("try"))
         .body("_embedded.terms[1].synonyms", hasSize(20))
         .body("_embedded.terms[1].synonyms", hasItem("test"))
-        .body("_embedded.terms[1]._links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/try"))
-        .body("_embedded.terms[1]._links.self.href", matchesPattern("http://[^/]+/api/v1/terms/" + tryId))
-        .body("_embedded.terms[1]._links.list.href", matchesPattern("http://[^/]+/api/v1/terms"))
+        .body("_embedded.terms[1]._links.self-by-word.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/try"))
+        .body("_embedded.terms[1]._links.self.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/" + tryId))
+        .body("_embedded.terms[1]._links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT))
         .body("_embedded.terms[2].id", equalTo(testId))
         .body("_embedded.terms[2].word", equalTo("test"))
         .body("_embedded.terms[2].synonyms", hasSize(16))
         .body("_embedded.terms[2].synonyms", hasItem("run"))
-        .body("_embedded.terms[2]._links.self-by-word.href", matchesPattern("http://[^/]+/api/v1/terms/test"))
-        .body("_embedded.terms[2]._links.self.href", matchesPattern("http://[^/]+/api/v1/terms/" + testId))
-        .body("_embedded.terms[2]._links.list.href", matchesPattern("http://[^/]+/api/v1/terms"))
-        .body("_links.list.href", matchesPattern("http://[^/]+/api/v1/terms"));
+        .body("_embedded.terms[2]._links.self-by-word.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/test"))
+        .body("_embedded.terms[2]._links.self.href", matchesPattern(DOMAIN + TERMS_ENDPOINT + "/" + testId))
+        .body("_embedded.terms[2]._links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT))
+        .body("_links.list.href", matchesPattern(DOMAIN + TERMS_ENDPOINT));
   }
 }
