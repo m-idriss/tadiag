cd2f0b973ea5bc6a16dd3c24c49f1956257484a1
Idriss MOHAMADY <pro.mohamady@gmail.com>
2024-07-29 15:11:35 +0200

    refactor: Remove unused import in StaticContentTest

--- src/test/java/com/dime/StaticContentTest.java
+++ src/test/java/com/dime/StaticContentTest.java
@@ -3,7 +3,6 @@ package com.dime;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
